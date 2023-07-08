9220487a474765d5b1ee36c6fd47c4653db36377
Anas Bensaid <bensaidanass9@gmail.com>
2023-07-08 15:54:26 +0100

    Adding the User model

---
+++ frontend/src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export class User {
+    id!: number;
+    firstName!: string;
+    lastName!: string;
+    email!: string;
+    password!: string;
+    isActive!: boolean;
+    role!: string;
+    phoneNumber!: string;
+    address!: string;
+    birthdate!: Date;
+}
